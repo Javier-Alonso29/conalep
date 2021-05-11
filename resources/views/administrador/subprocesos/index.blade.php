76cda8c6b84b66788104d9cd7a17a9383b387073
Eduardo Aguilar Yáñez <eduardoay115@gmail.com>
2021-05-11 01:05:43 -0500

    Se agregaron los metodos de updatebyProceso y destroybyProceso

--- resources/views/administrador/subprocesos/index.blade.php
+++ resources/views/administrador/subprocesos/index.blade.php
@@ -72,7 +72,6 @@
                             <th>Nombre</th>
                             <th>Codigo</th>
                             <th>Proceso</th>
-                            <th>Tipo de documento</th>
                             <th>Operaciones</th>
                         </tr>
                     </thead>
@@ -83,11 +82,6 @@
                             <td>{{  $subproceso->nombre  }}</td>
                             <td>{{  $subproceso->codigo  }}</td>
                             <td>{{  $subproceso->proceso['nombre']  }}</td>
-                            <td>
-                                <a class="btn btn-success btn-circle btn-sm" href="#" role="button">
-                                    <i class="fas fa-angle-double-right"></i>
-                                </a>
-                            </td>
                             <td>
                                 <a class="btn btn-danger btn-circle btn-sm" data-toggle="modal" data-target="#eliminar"  href="#" data-datos="{{$subproceso}}">
                                     <i class="fa fa-trash" ></i>
