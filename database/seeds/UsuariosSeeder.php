a80f471ff73683b968ef74f59ac4e72e8b430e3a
SPEKTRE115 <eduardoay115@gmail.com>
2021-06-22 12:21:22 -0500

    Modificacion de UsuariosSeeder para un superusuario por cada municipio

--- database/seeds/UsuariosSeeder.php
+++ database/seeds/UsuariosSeeder.php
@@ -17,15 +17,39 @@ class UsuariosSeeder extends Seeder
         $rol_administrador = DB::table('roles')->where('id',2)->value('id');
         $rol_SU_Estatal = DB::table('roles')->where('id',3)->value('id');
         $plantel = DB::table('planteles')->where('id',1)->value('id');
+        $plantel_2 = DB::table('planteles')->where('id',2)->value('id');
+        $plantel_3 = DB::table('planteles')->where('id',3)->value('id');
 
         DB::table('users')->insert(
-            ['name'=>'Test',
-            'apellido_paterno'=>'Test',
-            'apellido_materno'=>'Test',
+            ['name'=>'Super',
+            'apellido_paterno'=>'Usuario',
+            'apellido_materno'=>'Uno',
             'email'=>'test@zac.conalep.edu.mx',
             'password'=>Hash::make('testtesttest'),
             'rol_id'=>$rol_super_usuario,
             'id_plantel' => $plantel
+            ]
+        );
+
+        DB::table('users')->insert(
+            ['name'=>'Super',
+            'apellido_paterno'=>'Usuario',
+            'apellido_materno'=>'Dos',
+            'email'=>'superusuario_plantel2@zac.conalep.edu.mx',
+            'password'=>Hash::make('testtesttest'),
+            'rol_id'=>$rol_super_usuario,
+            'id_plantel' => $plantel_2
+            ]
+        );
+
+        DB::table('users')->insert(
+            ['name'=>'Super',
+            'apellido_paterno'=>'Usuario',
+            'apellido_materno'=>'Tres',
+            'email'=>'superusuario_plantel3@zac.conalep.edu.mx',
+            'password'=>Hash::make('testtesttest'),
+            'rol_id'=>$rol_super_usuario,
+            'id_plantel' => $plantel_3
             ]
         );
 
